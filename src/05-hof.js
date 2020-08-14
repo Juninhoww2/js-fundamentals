a95ddf0f7a9e4db6a0cf336588c108af9e33ad70
alqadiano96 <alqadianoKira@gmail.com>
2020-08-14 15:46:03 -0300

    master

--- src/05-hof.js
+++ src/05-hof.js
@@ -1,1 +1,18 @@
 // high order function
+
+const soma = (a, b) => a + b
+const mult = (a, b) => a * b
+
+const calc = (op, a, b) => op(a, b)
+const selectOp = op => {
+  if(op === '+'){
+     return soma{
+  }
+  if(op === '*'){
+     return mult
+}
+const c = calc(soma, 10, 5)
+console.log(c)
+
+const d = calc(selectOp'+', 10, 5 )
+console.log(d)
